8a87337844416de792bffb59c3c8d15cedbda810
zhangzy <490733118@qq.com>
2019-09-18 15:41:52 +0800

    html plugin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
+import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
+
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  btn.innerHTML = 'Click';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
